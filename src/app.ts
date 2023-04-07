fac6b084ef0e1351fff5d446ac490b3fedcec348
Carla Valle <carla.valle@engenharia.ufjf.br>
2023-04-07 15:18:16 -0300

    commit init

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import express, { Express } from "express";
+import cors from "cors";
+
+import { connectDb, disconnectDB } from "./config/database";
+
+const app = express();
+app
+  .use(cors())
+  .use(express.json())
+
+export async function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
+
+export default app;
